5ff8ab649fe97db4d84f6961b9e8093e34cadeb5
Matias Bella <matias.bella@hotmail.com>
2024-06-12 13:19:16 -0300

    Ahora el nodo mantiene todo el estado

--- jardin_backtracking.py
+++ jardin_backtracking.py
@@ -1,39 +1,86 @@
-B = [[9, 0, 12, 5], [0, 20, 0, 0], [4, 0, 1, 9]]
-patrones_prohibidos = [[[1, 0, 1], [0, 0, 0], [1, 0, 1]]]
+# B = [
+#     [9, 0, 12, 5],
+#     [0, 20, 0, 0],
+#     [4, 0, 1, 9],
+# ]
+B = [
+    [8, 1],
+    [1, 5],
+]
+patrones_prohibidos = [
+    [
+        [1, 0, 1],
+        [0, 0, 0],
+        [1, 0, 1],
+    ],
+]
 
 
 class Nodo:
-    def __init__(self, jardin_estado, sum_belleza: int):
-        self.jardin_estado = jardin_estado
+    def __init__(self, cords, jardin_estado, sum_belleza: int, belleza, celdas_prohib):
+        self.jardin_estado: list[list[int]] = jardin_estado
+        self.cords = cords
         self.sum_belleza = sum_belleza
+        self.belleza = belleza
+        self.celdas_prohibidas: set[tuple[int, int]] = celdas_prohib
         self.hijos: list["Nodo"] = []
+
+    def __str__(self, nivel=0) -> str:
+        nodo_str = (
+            repr(self.cords)
+            + "Belleza de celda: "
+            + repr(self.belleza)
+            + " Sumatoria de bellezas: "
+            + repr(self.sum_belleza)
+        )
+        rep = "\t" * nivel + nodo_str + "\n"
+        for hijo in self.hijos:
+            rep += hijo.__str__(nivel + 1)
+        return rep
 
     def nuevo_hijo(self, nodo):
         self.hijos.append(nodo)
 
 
-def belleza(jardin, patrones_prohib) -> int:
+def belleza(jardin, patrones_prohib):
     filas = len(jardin)
     cols = len(jardin[0])
     raiz = Nodo(
-        jardin_estado=[[0] for _ in range(cols) for _ in range(filas)],
+        cords=None,
+        jardin_estado=[[0 for _ in range(cols)] for _ in range(filas)],
         sum_belleza=0,
+        belleza=0,
+        celdas_prohib=set(),
     )
-    celdas_prohibidas: set[tuple[int, int]] = set()
-    generar_soluciones(raiz, celdas_prohibidas, jardin)
+    generar_soluciones(
+        padre=raiz,
+        bellezas=jardin,
+    )
+    print(raiz)
 
 
-def generar_soluciones(padre: "Nodo", cel_prohibidas, bellezas, fila=0, col=0):
-    if col >= len(bellezas[0]) - 1:
-        if fila >= len(bellezas) - 1:
+def generar_soluciones(padre: "Nodo", bellezas, fila=0, col=0):
+    if col >= len(bellezas[0]):
+        if fila >= len(bellezas):
             return
         col = 0
         fila += 1
+
     for fila in range(fila, len(bellezas)):
         for col in range(col, len(bellezas[0])):
-            if es_celda_prohibida(fila, col, cel_prohibidas):
+            print("fila", fila)
+            print("columna", col)
+            print("prohibidas", padre.celdas_prohibidas)
+            if (fila, col) in padre.celdas_prohibidas:
                 continue
-            nuevas_prohibidas = cel_prohibidas.update(
+            nuevo_nodo = Nodo(
+                cords=(fila, col),
+                jardin_estado=padre.jardin_estado,
+                sum_belleza=padre.sum_belleza + bellezas[fila][col],
+                belleza=bellezas[fila][col],
+                celdas_prohib=padre.celdas_prohibidas,
+            )
+            nuevo_nodo.celdas_prohibidas.update(
                 [
                     (fila + 1, col + 1),
                     (fila, col + 1),
@@ -43,16 +90,15 @@ def generar_soluciones(padre: "Nodo", cel_prohibidas, bellezas, fila=0, col=0):
                     (fila - 1, col),
                 ]
             )
-            nuevo_estado_jardin = padre.jardin_estado
-            nuevo_estado_jardin[fila, col] = bellezas[fila, col]
-            nuevo_nodo = Nodo(
-                jardin_estado=nuevo_estado_jardin,
-                sum_belleza=padre.sum_belleza + bellezas[fila, col],
-            )
+            nuevo_nodo.jardin_estado[fila][col] = bellezas[fila][col]
             padre.nuevo_hijo(nuevo_nodo)
-            generar_soluciones(nuevo_nodo, nuevas_prohibidas, bellezas, fila, col + 1)
+            generar_soluciones(
+                nuevo_nodo,
+                bellezas,
+                fila,
+                col + 1,
+            )
         col = 0
 
 
-def es_celda_prohibida(fila, col, cel_prohibidas) -> bool:
-    return (fila, col) in cel_prohibidas
+belleza(B, None)
